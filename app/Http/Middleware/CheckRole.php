6412f73ca3bc3d84e20f4ef75e4e59b2956243fb
abianHakim <abianhakim09@gmail.com>
2025-02-20 01:13:54 +0700

    menambahkan Middleware

---
+++ app/Http/Middleware/CheckRole.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, ...$roles)
+    {
+
+        if (!Auth::check()) {
+            return redirect('login');
+        }
+
+        if (!in_array(Auth::user()->role, $roles)) {
+            Log::info('Percobaan akses dengan peran tidak sah', ['role' => Auth::user()->role]);
+            return redirect()->route('dashboard');
+        }
+
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,40 @@
 
 use App\Http\Controllers\authController;
 use App\Http\Controllers\kategoriController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('login');
 });
 
+Route::get('/dashboard', function () {
+    if (Auth::check()) {
+        // Cek role user
+        if (Auth::user()->role === 'admin') {
+            return redirect()->route('kategori.index');
+        }
+        if (Auth::user()->role === 'kasir') {
+            return redirect()->route('kategori.index');
+        }
+        return view('dashboard');
+    }
+    return redirect()->route('login');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
 Route::get('login', [authController::class, 'showLoginForm'])->name('login');
 Route::post('login', [authController::class, 'login'])->name('cek-login');
 Route::post('logout', [authController::class, 'logout'])->name('logout');
 
-Route::get('kategori', [kategoriController::class, 'index'])->name('kategori.index');
-Route::post('kategori', [kategoriController::class, 'store'])->name('kategori.store');
-Route::patch('/kategori/{id}', [KategoriController::class, 'update'])->name('kategori.update');
-Route::delete('/kategori/{id}', [KategoriController::class, 'destroy'])->name('kategori.destroy');
+
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    // kategori
+    Route::get('kategori', [kategoriController::class, 'index'])->name('kategori.index');
+    Route::post('kategori', [kategoriController::class, 'store'])->name('kategori.store');
+    Route::patch('/kategori/{id}', [KategoriController::class, 'update'])->name('kategori.update');
+    Route::delete('/kategori/{id}', [KategoriController::class, 'destroy'])->name('kategori.destroy');
+});
+
+
+Route::middleware(['role:kasir'])->group(function () {});
